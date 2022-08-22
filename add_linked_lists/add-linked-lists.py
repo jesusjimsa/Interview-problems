92c8180b78845a6c1aae926cfdf15a09e252e52e
jesusjimsa <jesusjimsa@icloud.com>
2022-08-22 19:11:18 +0100

    Fiix all pylint issues

--- add_linked_lists/add-linked-lists.py
+++ add_linked_lists/add-linked-lists.py
@@ -10,14 +10,23 @@ Example:
 
 
 # Definition for singly-linked list.
-class ListNode(object):
+class ListNode:
+    '''
+    ListNode class
+    '''
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
 class Solution:
-    def addTwoNumbers(self, l1, l2, c=0):
+    '''
+    Solution class
+    '''
+    def addTwoNumbers(self, l1, l2):
+        '''
+            Adds two numbers in the linked list
+        '''
         solution = ListNode(0)
         solution_next = solution
         node_l1 = l1
@@ -59,15 +68,15 @@ class Solution:
         return solution
 
 
-l1 = ListNode(2)
-l1.next = ListNode(4)
-l1.next.next = ListNode(3)
+list1 = ListNode(2)
+list1.next = ListNode(4)
+list1.next.next = ListNode(3)
 
-l2 = ListNode(5)
-l2.next = ListNode(6)
-l2.next.next = ListNode(4)
+list2 = ListNode(5)
+list2.next = ListNode(6)
+list2.next.next = ListNode(4)
 
-result = Solution().addTwoNumbers(l1, l2)
+result = Solution().addTwoNumbers(list1, list2)
 while result:
     print(result.val)
     result = result.next
