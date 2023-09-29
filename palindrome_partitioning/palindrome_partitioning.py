8e0dac2c45cdbf09f6fa0c52c3296237d608d34d
jesusjimsa <jesusjimsa@icloud.com>
2023-09-29 21:57:53 +0100

    Fix pylint issues

--- palindrome_partitioning/palindrome_partitioning.py
+++ palindrome_partitioning/palindrome_partitioning.py
@@ -21,47 +21,56 @@ palindrome partitioning of `s`_.
 """
 
 
-class Solution:
-    def partition(self, s: str):
-        result = list(list())
+def partition(s: str):
+    """
+    Returns all possible palindrome partitioning of `s`.
 
-        result.append(list(s))  # The first list contains each separate character
+    Parameters
+    ----------
+    s : str
+        String to partition.
+    Returns
+    -------
+    result : list(list(str))
+        List with a list of all possible palindrome partitioning of `s`.
+    """
+    result = list([])
 
-        if len(s) == 1:
-            return result
+    result.append(list(s))  # The first list contains each separate character
 
-        for i in range(2, len(s) + 1):
-            s_copy = s
-            tmp_list = list()
-            j = 0
-            max_tries = len(s) - i + 1
-
-            while len(s_copy) > 0 and j < len(s_copy) and max_tries > 0:
-                s_chunk = s_copy[j:j+i]
-
-                if s_chunk == s_chunk[::-1]:    # s_chunk == reversed(s_chunk)
-                    tmp_list.append(s_chunk)
-                    s_copy = s_copy.replace(s_chunk, "", 1)
-                else:
-                    j += 1
-
-                max_tries -= 1
-
-            if len(s_copy) > 0 and len(tmp_list) > 0:
-                for k in range(0, len(s_copy)):
-                    tmp_list.append(s_copy[k])
-
-                result.append(tmp_list)
-            elif len(tmp_list) > 0:
-                result.append(tmp_list)
-
+    if len(s) == 1:
         return result
 
+    for i in range(2, len(s) + 1):
+        s_copy = s
+        tmp_list = []
+        j = 0
+        max_tries = len(s) - i + 1
 
-sol = Solution()
+        while len(s_copy) > 0 and j < len(s_copy) and max_tries > 0:
+            s_chunk = s_copy[j:j+i]
 
-print(sol.partition("aab"))
-print(sol.partition("a"))
-print(sol.partition("bb"))
-print(sol.partition("cdd"))
-print(sol.partition("efe"))
+            if s_chunk == s_chunk[::-1]:    # s_chunk == reversed(s_chunk)
+                tmp_list.append(s_chunk)
+                s_copy = s_copy.replace(s_chunk, "", 1)
+            else:
+                j += 1
+
+            max_tries -= 1
+
+        if len(s_copy) > 0 and len(tmp_list) > 0:
+            for k in range(0, len(s_copy)):
+                tmp_list.append(s_copy[k])
+
+            result.append(tmp_list)
+        elif len(tmp_list) > 0:
+            result.append(tmp_list)
+
+    return result
+
+
+print(partition("aab"))
+print(partition("a"))
+print(partition("bb"))
+print(partition("cdd"))
+print(partition("efe"))
