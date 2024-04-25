c37cc3dc6e4e504ece3dc00642891201ae42d396
jesusjimsa <jesusjimsa@icloud.com>
2024-04-25 20:21:29 +0100

    Perm check

---
+++ perm_check/perm_check.py
@@ -0,0 +1,84 @@
+'''
+    A non-empty array `A` consisting of `N` integers is given.
+
+    A permutation is a sequence containing each element from `1` to `N` once, and only once.
+
+    For example, array `A` such that:
+    ```
+        A[0] = 4
+        A[1] = 1
+        A[2] = 3
+        A[3] = 2
+    ```
+
+    is a permutation, but array A such that:
+    ```
+        A[0] = 4
+        A[1] = 1
+        A[2] = 3
+    ```
+
+    is not a permutation, because value `2` is missing.
+
+    The goal is to check whether array `A` is a permutation.
+
+    Write a function:
+    ```
+    def solution(A)
+    ```
+
+    that, given an array `A`, returns `1` if array `A` is a permutation and `0` if it is not.
+
+    For example, given array `A` such that:
+    ```
+        A[0] = 4
+        A[1] = 1
+        A[2] = 3
+        A[3] = 2
+    ```
+
+    the function should return `1`.
+
+    Given array `A` such that:
+    ```
+        A[0] = 4
+        A[1] = 1
+        A[2] = 3
+    ```
+    the function should return 0.
+
+    Write an efficient algorithm for the following assumptions:
+
+    * `N` is an integer within the range `[1..100,000]`;
+    * Each element of array A is an integer within the range `[1..1,000,000,000]`.
+'''
+
+
+def solution(A):
+    '''
+    Check if A is a permutation.
+
+    Parameters
+    ----------
+    A : List
+        List to be checked.
+
+    Returns
+    -------
+    0 if it's not a permutation, 1 if it is a permutation.
+    '''
+    permutation = list(range(1, len(A) + 1))
+    A.sort()
+
+    if max(A) > 100000:
+        return 0
+
+    if permutation != A:
+        return 0
+
+    return 1
+
+
+arr = [4, 1, 3, 2]
+
+print(solution(arr))
